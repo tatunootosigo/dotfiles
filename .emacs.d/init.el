c693a7d56d09df57eabaae5f1a121b37bb5db3a0
tatunootosigo <traumaflower@gmail.com>
2024-04-03 12:55:34 +0900

    emacs conf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,6 +28,26 @@
 (menu-bar-mode -1)                              ;; メニューバーを非表示
 (setq-default show-trailing-whitespace t)       ;; 行末の空白を表示
 
+;; 全角空白の可視化
+(require 'whitespace)
+(setq whitespace-style '(face
+                         trailing
+                         tabs
+                         spaces
+                         empty
+                         space-mark
+                         tab-mark))
+(setq whitespace-display-mappings
+      '((space-mark ?\u3000 [?\u25a1])
+        ;; WARNING: the mapping below has a problem.
+        ;; When a TAB occupies exactly one column, it will display the
+        ;; character ?\xBB at that column followed by a TAB which goes to
+        ;; the next TAB column.
+        ;; If this is a problem for you, please, comment the line below.
+        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+(global-whitespace-mode 1)
+
 ;; 行数表示
 (global-linum-mode t)
 (defvar linum-padding-number 0) ;;行番号の頭に付ける空白の数
